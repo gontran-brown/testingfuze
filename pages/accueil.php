de0d86281e0efb6acda2a43457ee9c34a95e8895
soumia <adjir.soumia@infuze.fr>
2012-04-30 10:19:19 +0200

    grand changement en php

---
+++ pages/accueil.php
@@ -0,0 +1,27 @@
+      <div id='banniere'></div>
+      <div id='connexion'>
+        <ul>
+          <li>
+            <a href='#'>
+              <div>
+                se connecter a facebook
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href='index.php?page=inscription'>
+              <div>
+                s'inscrire
+              </div>
+            </a>
+          </li>
+          <li>
+            <a href='#'>
+              <div>
+                se connecter
+              </div>
+            </a>
+          </li>
+        </ul>
+      </div>
+
--- pages/inscription.php
+++ pages/inscription.php
@@ -1,13 +1,3 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html lang='fr' xml:lang='fr' xmlns='http://www.w3.org/1999/xhtml'>
-  <head>
-    <meta content='text/html; charset=utf-8' http-equiv='Content-Type' />
-    <meta content='Soumia ADJIR' name='autor' />
-    <meta content="Une avanche d'info sur les montagnes" name='description' />
-    <meta content='' name='keybords' />
-    <link href='../css/style.css' rel='stylesheet' type='text/css' />
-  </head>
-  <body>
     <div id='header'>
       <ul id='pass'>
         <li>
@@ -53,5 +43,3 @@
         </form>
       </div>
     </div>
-  </body>
-</html>
